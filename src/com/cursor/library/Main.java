556350f8ebaa4741542fc4885d63673d0a951e92
andriydovbenko <andriydovbenko1@gmail.com>
2019-12-19 10:22:24 +0100

    created Main.java class

---
+++ src/com/cursor/library/Main.java
@@ -0,0 +1,10 @@
+package com.cursor.library;
+
+import com.cursor.library.asistant.MyLibraryAssistant;
+
+public class Main {
+
+    public static void main(String[] args) {
+        new MyLibraryAssistant().executeAssistant();
+    }
+}
