0552bc05e0f07560a50cc249b5d1914ca38dc48e
andriydovbenko <andriydovbenko1@gmail.com>
2019-12-19 10:12:07 +0100

    init empty class Assistant

---
+++ src/com/cursor/library/asistant/MyLibraryAssistant.java
@@ -0,0 +1,12 @@
+package com.cursor.library.asistant;
+
+import com.cursor.library.inputs.InputChoiceProcessing;
+import com.cursor.library.manybook.LibraryManyBookPerDay;
+import com.cursor.library.onebook.LibraryOneBookPerDay;
+
+public class MyLibraryAssistant {
+    private String nameLibraryVisitor;
+
+    public void executeAssistant() {
+      
+}
